f24fb277e29e9fc8d95b8b9adf207c48d02d980f
Gabriela Taniguchi <110126586+gabitaniguchi@users.noreply.github.com>
2025-05-26 14:48:26 -0300

    Update coin collector.cpp

--- grafos/kosaraju/coin collector.cpp
+++ grafos/kosaraju/coin collector.cpp
@@ -1,52 +1,54 @@
 #include <bits/stdc++.h>
-#define int long long
-#define MAX (int)(1e5+10)
-#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+
 using namespace std;
 
-int n,m;
-int vis[MAX], custo[MAX], dp[MAX];
-vector<int> g[MAX], inv[MAX];
-set<int> filhos;
-stack<int> tp;
+#define MAX (int)(1e5+10)
+#define int long long
 
-void dfs(int x){
+int n, m, coin[MAX], vis[MAX], dp[MAX];
+vector<int> g[MAX], inv[MAX];
+set<int> scc;
+stack<int> tp;
+map<int, int> corresp;
+
+void top_sort(int x){
+
     vis[x] = 1;
-    for(auto next: g[x]){
-        if(vis[next]) continue;
-        dfs(next);
-    }
+    for(auto nxt: g[x])
+        if(!vis[nxt]) top_sort(nxt);
+    
     tp.push(x);
 }
 
-void kosaraju(int x){
+void kosaraju(int x, int raiz){
     vis[x] = 1;
 
-    for(auto next: inv[x]){
-        if(vis[next]) continue;
-        kosaraju(next);
-        filhos.insert(next);
+    for(auto nxt: inv[x]){
+        if(vis[nxt]) continue;
+        scc.insert(nxt);
+        kosaraju(nxt, raiz);
     }
 }
-void longest_path(int x){
+
+void maior_dist(int x){
     vis[x]=1;
 
-    for(auto next: g[x]){
-        if(vis[next]) continue;
-        longest_path(next);
-        dp[x] = max(dp[x], dp[next]+custo[x]);
+    for(auto nxt: g[x]){
+        if(!vis[corresp[nxt]]) maior_dist(corresp[nxt]);
+        dp[x] = max(dp[x], dp[corresp[nxt]] + coin[x]);
     }
 }
 
-int32_t main()
-{_
+int32_t main(){
 
     cin>>n>>m;
 
     for(int i=1; i<=n; i++) {
-        cin>>custo[i];
-        dp[i] = custo[i];
+        cin>>coin[i];
+        dp[i] = coin[i];
+        corresp[i] = i;
     }
+
     for(int i=1; i<=m; i++){
         int a,b;
         cin>>a>>b;
@@ -56,44 +58,53 @@ int32_t main()
     }
 
     for(int i=1; i<=n; i++){
-        if(!vis[i]) dfs(i);
+        if(vis[i]) continue;
+        top_sort(i);
     }
 
     memset(vis, 0, sizeof(vis));
 
-
     while(!tp.empty()){
-        int atual = tp.top();
-        filhos.clear();
+        int i= tp.top(); tp.pop();
+        if(vis[i]) continue;
+        scc.clear();
+        kosaraju(i, i);
 
-        if(!vis[atual]){
-            kosaraju(atual);
-            cout<<"DAG "<<atual<<" ";
-
-            for(auto next: filhos){
-                for(auto prox: g[next]){
-                    if(filhos.find(prox)==filhos.end()) 
-                        g[atual].push_back(prox);
-                }
-                cout<<next<<" ";
-                g[next].clear();
-                custo[atual] += custo[next];
-                dp[atual] = custo[atual];
+        for(auto x: scc){
+            if(x==i) continue;
+            corresp[x] = i;
+            for(auto nxt: g[x]){
+                if(corresp[nxt]==1 || nxt==i || scc.find(nxt)!=scc.end()) continue;
+                g[i].push_back(nxt);
             }
-            cout<<endl;
+            g[x].clear();
+            coin[i] += coin[x];
         }
-        tp.pop();
+        dp[i] = coin[i];
+
+        for(int j=0; j<(int)g[i].size();j++){
+            if(corresp[g[i][j]]==i) {
+                g[i].erase(g[i].begin()+j);
+                j--;
+            }
+        }
     }
 
-    memset(vis,0, sizeof(vis));
+    memset(vis, 0, sizeof(vis));
 
     for(int i=1; i<=n; i++){
-        if(!vis[i]) longest_path(i);
+        if(!vis[corresp[i]]) maior_dist(corresp[i]);
     }
 
     int ans =0;
-
-    for(int i=1; i<=n; i++) ans = max(ans, dp[i]);
-
+    for(int i=1; i<=n; i++) ans = max(ans, dp[corresp[i]]);
     cout<<ans<<endl;
+
+    // for(int i=1;i<=n; i++){
+    //     cout<<"V "<<i<<": ";
+    //     for(auto nxt:g[i]) cout<<nxt<<" ";
+    //     cout<<endl;
+    // }
+
+    return 0;
 }
